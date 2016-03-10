5240cb6fb95b3cda9a511164ab888123f456209c
BruceLiCN <289948587@qq.com>
2016-03-10 21:00:39 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,34 @@
 ## These two functions are quite similar to the two in the example.
 ## 
 
-## Just run it at the beginning.
+## Function to return the special matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## Set m to NULL
         m <- NULL
+        ## Set input value to set
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        ## Set get, setsolve and getsolve.  It's the same as the example.
         get <- function() x
         setsolve <- function(solve) m <<- solve
         getsolve <- function() m
+        
+        ## Return the special matrix
         list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
 }
 
+
+
+## Function to calculate Inverse of Matrix using solve()
+
 cacheSolve <- function(x, ...) {
         m <- x$getsolve()
+        ## check whether we can gat the cached inverse directly.
         if(!is.null(m)) {
                 message("getting cached inverse")
                 return(m)
@@ -26,5 +36,7 @@ cacheSolve <- function(x, ...) {
         data <- x$get()
         m <- solve(data, ...)
         x$setsolve(m)
+        
+        ##return the inverse
         m
 }
